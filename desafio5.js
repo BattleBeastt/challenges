8df42e167d1a23d93574cea0eb88ba006429f173
Michael <guayacosfeed@gmail.com>
2024-09-06 22:36:43 -0500

    update #7DaysOfCode

--- desafio5.js
+++ desafio5.js
@@ -1,23 +1,23 @@
 let listaDeCompra = [];
-let categorias = ["lacteos", "carnicos", "embutidos", "frutas", "verduras"];
-let lacteos = [];
-let carnicos = [];
-let embutidos = [];
-let frutas = [];
-let verduras = [];
+let categorias = [
+	{categoria: "lacteos", alimento: " "},
+	{categoria: "carnicos", alimento: " "},
+	{categoria: "embutidos", alimento: " "},
+	{categoria: "frutas", alimento: " "},
+	{categoria: "verduras", alimento: " "}
+	];
 let pregunta1 = prompt("¿deseas agregar un alimento a tu lista de compras?\n" + "SI\n" + "NO");
 
 while(pregunta1 === "si"){
 	let pregunta2 = listaDeCompra.push(prompt("¿que alimento deseas agregar?"));
 	let pregunta3 = prompt("¿en que categoria encaja ese alimento?\n" + "lacteos\n" + "carnicos\n" + "embutidos\n" + "frutas\n" + "verduras\n");
 	
-	if(pregunta3){
-		lacteos.push(pregunta2);
-	} else {
-		console.log("error al categorizar");
-	}
+	let nuevaLista = categorias.filter(function(product){
+			return product.alimento += //listaDeCompra;
+	});
+	
 	pregunta1 = prompt("¿deseas agregar un alimento a tu lista de compras?\n" + "SI\n" + "NO");
 }
 
 console.log(listaDeCompra);
-console.log(lacteos);
+console.log(categorias);
