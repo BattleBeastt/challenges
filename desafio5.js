f0d74ccabbb547e9b52a8e0ff397d6ca69192631
Michael <guayacosfeed@gmail.com>
2024-09-07 23:36:19 -0500

    desafio incompleto

--- desafio5.js
+++ desafio5.js
@@ -1,23 +1,25 @@
 let listaDeCompra = [];
-let categorias = [
-	{categoria: "lacteos", alimento: " "},
-	{categoria: "carnicos", alimento: " "},
-	{categoria: "embutidos", alimento: " "},
-	{categoria: "frutas", alimento: " "},
-	{categoria: "verduras", alimento: " "}
-	];
+let categoria = ["lacteos", "embutidos", "frutas", "verduras"];
 let pregunta1 = prompt("¿deseas agregar un alimento a tu lista de compras?\n" + "SI\n" + "NO");
 
 while(pregunta1 === "si"){
 	let pregunta2 = listaDeCompra.push(prompt("¿que alimento deseas agregar?"));
-	let pregunta3 = prompt("¿en que categoria encaja ese alimento?\n" + "lacteos\n" + "carnicos\n" + "embutidos\n" + "frutas\n" + "verduras\n");
+	let pregunta3 = prompt("¿a qué categoria pertenence ese alimento?\n" + "lacteos\n" + "embutidos\n" + "frutas\n" + "verduras\n").value;
 	
-	let nuevaLista = categorias.filter(function(product){
-			return product.alimento += //listaDeCompra;
-	});
+	if(pregunta3){
+		let nuevaLista = categoria.push();
+	} else{
+		console.log("categoria no encontrada");
+	}
 	
 	pregunta1 = prompt("¿deseas agregar un alimento a tu lista de compras?\n" + "SI\n" + "NO");
 }
 
 console.log(listaDeCompra);
-console.log(categorias);
+console.log("lista de compras: " + categoria);
+
+	// {categoria: "lacteos", alimento: " "},
+	// {categoria: "carnicos", alimento: " "},
+	// {categoria: "embutidos", alimento: " "},
+	// {categoria: "frutas", alimento: " "},
+	// {categoria: "verduras", alimento: " "}
