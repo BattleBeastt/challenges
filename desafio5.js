0a701e36f2fc1dd6abea704f0d92f2f9fe5b0ddb
Michael <guayacosfeed@gmail.com>
2024-09-10 10:00:46 -0500

    update #7DaysOfCode complete

--- desafio5.js
+++ desafio5.js
@@ -1,4 +1,4 @@
-let listaDeCompra = [];
+let listaDeCompra = "";
 let lacteos = [];
 let embutidos = [];
 let frutas = [];
@@ -9,22 +9,29 @@ let categoria = "";
 let pregunta1 = prompt("¿deseas agregar un alimento a tu lista de compras?\n" + "si\n" + "no");
 
 while(pregunta1 != "no"){
-	
 	while(pregunta1 != "si" && pregunta1 != "no"){
 		alert("operacion no reconocida");
+		pregunta1 = prompt("¿deseas agregar un alimento a tu lista de compras?\n" + "si\n" + "no");
 	}
 	if(pregunta1 === "no"){
 		break;
 	}
-	let pregunta2 = listaDeCompra.push(prompt("¿que alimento deseas agregar?"));
-	let pregunta3 = prompt("¿a qué categoria pertenence ese alimento?");
-	if(pregunta3 == "lacteos"){
-		lacteos.push(pregunta2);
-	}
-	
-	
+	listaDeCompra = prompt("¿que alimento deseas agregar?");
+	categoria = prompt("¿a qué categoria pertenence ese alimento?");
+	if(categoria == "lacteos"){
+		lacteos.push(listaDeCompra);
+	} else if(categoria == "embutidos"){
+		embutidos.push(listaDeCompra);
+	} else if(categoria == "frutas"){
+		frutas.push(listaDeCompra);
+	} else if(categoria == "verduras"){
+		verduras.push(listaDeCompra);
+	} else if(categoria == "carnes"){
+		carnes.push(listaDeCompra);
+	} else{
+		alert("categoria no reconocida");
+	}	
 	pregunta1 = prompt("¿deseas agregar un alimento a tu lista de compras?\n" + "si\n" + "no");
 }
 
-console.log(listaDeCompra);
-console.log("lista de compras: " + lacteos);
+console.log(`lista de compras:\n lacteos:${lacteos}\n embutidos:${embutidos}\n frutas:${frutas}\n verduras:${verduras}\n carnes:${carnes}`);
