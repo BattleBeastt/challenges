7353c7e5edb6e34ea1fce82dda3c588c70531c15
Michael <guayacosfeed@gmail.com>
2024-10-17 22:20:08 -0500

    de practicasJava

---
+++ Computadoras.java
@@ -0,0 +1,10 @@
+package practicasJava;
+
+public class Computadoras {
+	public String marca;
+	public int pantalla;
+	public String procesador;
+	public int memoriaRam;
+	public int memoriaSolida;
+	
+}
